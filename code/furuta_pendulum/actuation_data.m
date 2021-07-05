84bd864c6d2b9410986fa8da0fab05c837c024b9
BrindhaJeniefer-Joseph <67753756+BrindhaJeniefer-Joseph@users.noreply.github.com>
2021-07-05 23:42:59 +0200

    Update actuation_data.m

--- code/furuta_pendulum/actuation_data.m
+++ code/furuta_pendulum/actuation_data.m
@@ -1,6 +1,6 @@
 function [ad] = actuation_data(u)
     
-    if( u < 1) % actuation successful
+    if( u < 1) % this is just the skeleton, function should return a 0 or 1 depending on something having gone wrong (0) or having been successful (1).
         ad = 1;
     else
         ad = 0;
