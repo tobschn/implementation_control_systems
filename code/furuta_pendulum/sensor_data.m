bdd9c1c834886a2ec2f7cf73d2f320e91d9cd0b8
BrindhaJeniefer-Joseph <67753756+BrindhaJeniefer-Joseph@users.noreply.github.com>
2021-07-14 16:04:28 +0200

    removing unwanted file

--- code/furuta_pendulum/sensor_data.m
+++
@@ -1,8 +0,0 @@
-function [sd] = sensor_data(y)
-    
-    if( y<1 ) % this is just the skeleton, function should return a 0 or 1 depending on something having gone wrong (0) or having been successful (1).
-        sd = 1;
-    else
-        sd = 0;
-    end    
-end
